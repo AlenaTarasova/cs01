019496c8e8d1ec5eba8c4f81f7356f91b1a2c616
Alema T <kyklyoshka.kisyy@mail.ru>
2022-09-06 12:58:27 +0300

    commit

--- Program.cs
+++ Program.cs
@@ -1,24 +1,27 @@
-﻿Console.WriteLine("Введите два числа");
+﻿Console.WriteLine("Введите три числа");
 /*
 string num1 = Console.ReadLine();
 string num2 = Console.ReadLine();
+string num3 = Console.ReadLine();
 var iNum1 = int.Parse(num1);
 var iNum2 = int.Parse(num2);
-var iNum2 = int.Parse(num2); 
+var iNum3 = int.Parse(num3);
+
 */
 int iNum1 = int.Parse(Console.ReadLine());
 int iNum2 = int.Parse(Console.ReadLine());
-if (iNum1 > iNum2)
-{
-    Console.WriteLine("Первое число больше");
-}
-else if (iNum1 < iNum2)
-{
-    Console.WriteLine("Второе число больше");
-}
-else
-{
-    Console.WriteLine("Числа равны");
-}
+int iNum3 = int.Parse(Console.ReadLine());
+int max = iNum1;
 
-
+if ( max < iNum2)
+if (max < iNum2)
+{
+max = iNum2;
+    max = iNum2;
+}
+if ( max < iNum3)
+if (max < iNum3)
+{
+    max = iNum3;
+}
+Console.WriteLine($"{max} тут самое большое");
